71d80553f5ec89fc321ec4e70d860bb0ffc39422
thadeveloper <samsondamilola22@gmail.com>
2020-03-13 12:27:43 +0100

    My laravel tutorial

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class PostsController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    public function create()
+    {
+        return view('posts/create');
+    }
+    public function store()
+    { 
+        $data = request()->validate([
+            'caption' => 'required',
+            'image' => ['required', 'image'],
+        ]);
+
+        $imagePaths = request('image')->store('uploads', 'public');
+        $image  = Image::make(public_path('storage/'. $imagePaths))-fit(1200, 1200);
+        $image->save();
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePaths,
+        ]);
+
+        return redirect('/profile/'. auth()->user()->id);
+    }
+}
